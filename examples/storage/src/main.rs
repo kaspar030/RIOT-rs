ea70a7c9874042bbe832e5faf6fa45ec88c649fd
Kaspar Schleiser <kaspar@schleiser.de>
2024-10-19 12:15:46 +0200

    fixup! feat(example): add storage example

--- examples/storage/src/main.rs
+++ examples/storage/src/main.rs
@@ -9,6 +9,7 @@ use riot_rs::storage;
 
 use serde::{Deserialize, Serialize};
 
+// some object. for storing it, derive the serde Serialize / Deserialize traits.
 #[derive(Serialize, Deserialize, Debug, defmt::Format)]
 struct MyConfig {
     val_uno: heapless::String<64>,
@@ -18,31 +19,81 @@ struct MyConfig {
 #[riot_rs::task(autostart)]
 async fn main() {
     info!("Hello from storage test!");
-    //storage::del("counter").await.unwrap();
+
+    // counter
     let value: Option<u32> = storage::get("counter").await.unwrap();
     let value = if let Some(value) = value {
-        info!("got value {}", value);
+        info!("got counter value {}", value);
         value
     } else {
+        info!("no counter value. first time running this test? try rebooting.");
         0
     };
 
-    let cfg = MyConfig {
-        val_uno: heapless::String::<64>::try_from("mybarrocks").unwrap(),
-        val_dos: 99,
-    };
     storage::put("counter", value + 1).await.unwrap();
-    storage::put("my_config", cfg).await.unwrap();
 
     {
+        // By getting the Storage mutex directly, changing e.g., a counter,
+        // can be done atomically w.r.t. concurrent access from the same firmware:
         let mut s = storage::get_mutex().await;
         let value: Option<u32> = s.get("counter").await.unwrap();
         let value = value.unwrap_or_default();
         s.put("counter", value + 1).await.unwrap();
     }
 
+    // storing a string value
+    // here, a literal can be used.
+    storage::put("string_key", "string_value").await.unwrap();
+    // getting a string value
+    if let Some(string) = storage::get::<heapless::String<64>>("string_key")
+        .await
+        .unwrap()
+    {
+        info!("got heapless string value {}", string);
+    }
+    if let Some(string) = storage::get::<arrayvec::ArrayString<64>>("string_key")
+        .await
+        .unwrap()
+    {
+        info!("got arrayvec string value with len {}", string.len());
+    }
+
+    // storing an object
+    let cfg = MyConfig {
+        val_uno: heapless::String::<64>::try_from("some value").unwrap(),
+        val_dos: 99,
+    };
+    storage::put("my_config", cfg).await.unwrap();
+
+    // getting an object. type needs to match.
     let cfg: Option<MyConfig> = storage::get("my_config").await.unwrap();
     if let Some(cfg) = cfg {
         info!("got cfg {:?}", cfg);
+    }
+
+    // getting a value as raw bytes returns probably not what you want due to the way
+    // postcard works
+    let cfg_array: Option<arrayvec::ArrayVec<u8, 256>> = storage::get("my_config").await.unwrap();
+    if let Some(cfg) = cfg_array.as_ref() {
+        info!("got cfg as arrayvec: {:x}", cfg.as_slice());
+    }
+
+    // same for byte arrays
+    let cfg_array: Option<[u8; 10]> = storage::get("my_config").await.unwrap();
+    if let Some(cfg) = cfg_array.as_ref() {
+        info!("got cfg as array: {:x}", cfg);
+    }
+
+    // raw bytes
+    let bytes: [u8; 5] = [0, 1, 2, 3, 4];
+    storage::put("some_raw_bytes", bytes).await.unwrap();
+
+    let bytes: Option<[u8; 5]> = storage::get("some_raw_bytes").await.unwrap();
+    if let Some(bytes) = bytes.as_ref() {
+        info!("got bytes as array: {:x}", bytes);
+    }
+    let bytes: Option<heapless::Vec<u8, 256>> = storage::get("some_raw_bytes").await.unwrap();
+    if let Some(bytes) = bytes.as_ref() {
+        info!("got bytes as heapless vec arr: {:x}", bytes);
     }
 }
