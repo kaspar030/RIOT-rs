b332a8321867a6749791106aab3883c06ae26def
Kaspar Schleiser <kaspar@schleiser.de>
2024-10-19 12:15:46 +0200

    fixup! feat(storage): introduce storage API

--- src/riot-rs-storage/src/lib.rs
+++ src/riot-rs-storage/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(test), no_std)]
 
+///! Key/Value configuration storage on flash.
 use core::ops::Range;
 use embassy_sync::{
     blocking_mutex::raw::CriticalSectionRawMutex,
@@ -59,10 +60,9 @@ pub fn init(p: &mut OptionalPeripherals) {
     let _ = STORAGE.init(Mutex::new(Storage::new(flash, flash_range)));
 }
 
-pub async fn get_mutex() -> MutexGuard<'static, CriticalSectionRawMutex, storage::Storage<Flash>> {
-    STORAGE.get().await.lock().await
-}
-
+/// Stores a key-value pair into flash memory.
+///
+/// It will overwrite the last value that has the same key.
 pub async fn put<'d, V>(key: &str, value: V) -> Result<(), sequential_storage::Error<FlashError>>
 where
     V: Serialize + Deserialize<'d> + Into<PostcardValue<V>>,
@@ -70,6 +70,11 @@ where
     get_mutex().await.put::<V>(key, value).await
 }
 
+/// Gets the last stored value from the flash that is associated with the given key.
+///
+/// Note: `get()` the same value type that was `put()`!
+///
+/// If no value with the key is found, None is returned.
 pub async fn get<V>(key: &str) -> Result<Option<V>, sequential_storage::Error<FlashError>>
 where
     V: Serialize + for<'d> Deserialize<'d> + Into<PostcardValue<V>>,
@@ -77,6 +82,39 @@ where
     get_mutex().await.get(key).await
 }
 
+/// Deletes an item from flash.
+///
+/// Additional calls to [`get()`] with the same key will return None until
+/// a new one is stored again.
+///
+/// <div class="warning">
+/// This is really slow!
+///
+/// All items in flash have to be read and deserialized to find the items with the key.
+/// This is unlikely to be cached well.
+/// </div>
 pub async fn del(key: &str) -> Result<(), sequential_storage::Error<FlashError>> {
     get_mutex().await.del(key).await
+}
+
+/// Gets a MutexGuard of the global `Storage` object.
+///
+/// This can be used to implement atomic RMW (like counters).
+///
+/// Note: don't forget to drop the mutex guard returned by this.
+///
+/// Example:
+///
+/// ```Rust
+///    {
+///        // By getting the Storage mutex directly, changing e.g., a counter,
+///        // can be done atomically w.r.t. concurrent access from the same firmware:
+///        let mut s = storage::get_mutex().await;
+///        let value: Option<u32> = s.get("counter").await.unwrap();
+///        let value = value.unwrap_or_default();
+///        s.put("counter", value + 1).await.unwrap();
+///    }
+/// ```
+pub async fn get_mutex() -> MutexGuard<'static, CriticalSectionRawMutex, storage::Storage<Flash>> {
+    STORAGE.get().await.lock().await
 }
